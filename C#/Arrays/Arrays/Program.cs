31b5d719f3a146001265d2e881b712e3f4112ff6
Altay <73247832+Spaktra@users.noreply.github.com>
2022-07-05 22:47:57 +0300

    C# Arrays

---
+++ C#/Arrays/Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Ahmet";
+            students[1] = "Altay";
+            students[2] = "Ayşenur";
+
+            string[] students2 = new[] {"Ahmet", "Altay", "Ayşenur"};
+
+            string[] students3 ={ "Ahmet", "Altay", "Ayşenur" };
+
+            foreach (var student in students3)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul", "İzmit","Balıkesir" },
+                {"Ankara", "Konya", "Kırıkkale" },
+                { "Antalya", "Adana", "Mersin" },
+                { "Rize", "Trabzon", "Mersin" },
+                { "İzmir", "Muğla", "Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0) ; i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("---------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
