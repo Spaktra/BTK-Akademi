c51d484f7e703acc5189f4b8b3bed813f84611eb
Altay <73247832+Spaktra@users.noreply.github.com>
2022-07-07 00:55:22 +0300

    C# Classes

---
+++ C#/Classes/Classes/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        //FIELD
+        //public string FirstName;
+
+        //PROPERTY
+        public int ID { get; set; }
+
+        //private string _firstName;
+        //public string FirstName
+        //{
+        //    get { return "Mr." + _firstName; }
+        //    set { _firstName = value; }
+        //}
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ C#/Classes/Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager(); 
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "İstanbul";
+            customer.ID = 1;
+            customer.FirstName = "Ahmet";
+            customer.LastName = "Altay";
+
+            Customer customer2 = new Customer 
+            {
+                ID = 2,
+                City = "Ankara",
+                FirstName = "Eren",
+                LastName = "Altay",
+            };
+
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadKey();
+        }
+    }
+}
