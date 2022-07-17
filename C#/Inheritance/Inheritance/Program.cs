fed25551288061191454359f0260f2d6596a33e7
Altay <73247832+Spaktra@users.noreply.github.com>
2022-07-17 22:25:12 +0300

    C# Inheritance
    C# Inheritance

---
+++ C#/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[]
+            {
+                new Customer{FirstName = "Ahmet"}, 
+                new Student{FirstName = "Salih"}, 
+                new Person{FirstName = "Mustafa"}
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+    
+}
